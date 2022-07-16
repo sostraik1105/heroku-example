5d4d524d568ee029edc7683467905bfb33e67551
sostraik1105 <83390692+sostraik1105@users.noreply.github.com>
2022-07-16 14:16:39 -0500

    corrected email transport

--- utils/email.util.js
+++ utils/email.util.js
@@ -11,7 +11,7 @@ class Email {
 
   // Connect to mail service
   newTransport() {
-    if (process.env.NODE_ENV === "development") {
+    if (process.env.NODE_ENV === "production") {
       return nodemailer.createTransport({
         service: "SendGrid",
         auth: {
